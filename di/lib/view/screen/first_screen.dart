5f05a435d0c05313b90f3ea651863615fbb0e6f5
favourTy <favouradetayo03@gmail.com>
2024-04-15 09:38:21 +0100

    fix

--- di/lib/view/screen/first_screen.dart
+++ di/lib/view/screen/first_screen.dart
@@ -1,7 +1,7 @@
 import 'package:di/controller/home_page_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
+//import 'package:get/get_state_manager/get_state_manager.dart';
 
 class FirstScreen extends StatelessWidget {
    FirstScreen({super.key});
--- getstorage/lib/view/screens/home_page.dart
+++ getstorage/lib/view/screens/home_page.dart
@@ -4,7 +4,6 @@ import 'first_screen.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
